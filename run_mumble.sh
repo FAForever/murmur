2dc5752d5c0f21e6515c8264a5fec226f62dbd70
Michel Jung <michel.jung89@gmail.com>
2017-01-26 17:00:38 +0100

    Clean up

--- run_mumble.sh
+++ run_mumble.sh
@@ -1,20 +1,22 @@
 env
-cat >> /etc/mumble-server.ini <<EOF
+cat <<EOF >> /etc/mumble-server.ini
+database=$DB_NAME
 dbDriver=QMYSQL
 dbUsername=$DB_USER
 dbPassword=$DB_PASSWORD
-dbHost=$DB_PORT_3306_TCP_ADDR
+dbHost=$DB_HOST
 dbPort=3306
 dbPrefix=
 dbOpts=
 EOF
+
 /usr/sbin/murmurd -supw $SUPERUSER_PASSWORD
-while true
-do
-if pgrep -f murmurd 1> /dev/null;then
-        sleep 1
-else
-        echo SERVER CRASHED
-	/usr/sbin/murmurd -v -fg
-fi
+
+while true; do
+  if pgrep -f murmurd 1> /dev/null; then
+    sleep 1
+  else
+    echo SERVER CRASHED
+    /usr/sbin/murmurd -v -fg
+  fi
 done
